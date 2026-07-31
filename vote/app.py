9281fce022e84b1a1eab528a3e927aaa2523c25a
Penchal Jammula <penchalj.devops@gmail.com>
2026-07-31 22:48:52 +0000

    update the source code file to verify Agrocd(GITOPS) intgeration
    Updated comment to include 'test' for clarupdate the source code file to verify Agrocd(GITOPS) intgerationity.

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 import logging
 
-# Read options and Redis configuration from environment variables
+# Read options and Redis configuration from environment variables test
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
 redis_host = os.getenv('REDIS_HOST', 'redis')  # Default to 'redis' for compatibility
